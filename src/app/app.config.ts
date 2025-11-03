93329872a48982ef8f889b57f8b9f7c807da17f5
omnyam7md <omnyam7md@gmail.com>
2025-11-04 02:46:33 +0400

    Enable HashLocationStrategy for Vercel

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-        provideRouter(routes, withHashLocation()),
+    provideRouter(routes, withHashLocation()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
